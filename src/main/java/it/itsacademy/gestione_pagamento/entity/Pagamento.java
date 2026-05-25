b31e034af72425d77b17a7e6850a74cbd2c74b2f
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:15:09 +0200

    FEAT: add new entity

---
+++ src/main/java/it/itsacademy/gestione_pagamento/entity/Pagamento.java
@@ -0,0 +1,25 @@
+package it.itsacademy.gestione_pagamento.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Pagamento")
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoPagamento statoPagamento;
+
+}
