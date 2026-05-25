3a7c40039311ae157fc2c6a8c5f33528eadc7f0b
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:28:05 +0200

    FIX: integer into UUID

--- src/main/java/it/itsacademy/gestione_pagamento/repository/PagamentoRepository.java
+++ src/main/java/it/itsacademy/gestione_pagamento/repository/PagamentoRepository.java
@@ -3,5 +3,7 @@ package it.itsacademy.gestione_pagamento.repository;
 import it.itsacademy.gestione_pagamento.entity.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+import java.util.UUID;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, UUID> {
 }
