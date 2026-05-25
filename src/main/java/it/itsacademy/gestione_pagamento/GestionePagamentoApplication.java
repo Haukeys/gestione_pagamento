033ec2f533616da014be42ecaafd5b1d6a5531c5
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:12:06 +0200

    Initial commit

---
+++ src/main/java/it/itsacademy/gestione_pagamento/GestionePagamentoApplication.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestione_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/itsacademy/gestione_pagamento/GestionePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsacademy.gestione_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
