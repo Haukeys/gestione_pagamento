227995226334522e07392e5a5d79b96fcba81cc1
Haukeys <leopazienza76@gmail.com>
2026-05-25 12:14:45 +0200

    FEAT: add info

--- src/main/java/it/itsacademy/gestione_pagamento/GestionePagamentoApplication.java
+++ src/main/java/it/itsacademy/gestione_pagamento/GestionePagamentoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestionePagamentoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionePagamentoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestionePagamentoApplication.class, args);
+    }
 
 }
--- src/test/java/it/itsacademy/gestione_pagamento/GestionePagamentoApplicationTests.java
+++ src/test/java/it/itsacademy/gestione_pagamento/GestionePagamentoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GestionePagamentoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
