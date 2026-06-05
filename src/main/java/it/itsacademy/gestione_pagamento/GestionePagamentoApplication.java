2e27196c1424fe52e20f3c2198397a01ac89bc93
Haukeys <leopazienza76@gmail.com>
2026-06-05 11:23:16 +0200

    FEAT: add enablescheduling for the scheduler

--- src/main/java/it/itsacademy/gestione_pagamento/GestionePagamentoApplication.java
+++ src/main/java/it/itsacademy/gestione_pagamento/GestionePagamentoApplication.java
@@ -2,8 +2,10 @@ package it.itsacademy.gestione_pagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // A METTRE ABSOLUMENT  SI ON VEUX QUE LE SCHEDULER FONCTIONE QUAND ON LANCE L APPLICATION
 public class GestionePagamentoApplication {
 
     public static void main(String[] args) {
