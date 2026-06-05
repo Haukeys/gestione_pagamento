9bc5b4292765c519856266c7f60f0c3350cde449
Haukeys <leopazienza76@gmail.com>
2026-06-05 11:22:24 +0200

    FEAT: add query name to erase pagamento rifiutato

--- src/main/java/it/itsacademy/gestione_pagamento/repository/PagamentoRepository.java
+++ src/main/java/it/itsacademy/gestione_pagamento/repository/PagamentoRepository.java
@@ -1,13 +1,20 @@
 package it.itsacademy.gestione_pagamento.repository;
 
 import it.itsacademy.gestione_pagamento.entity.Pagamento;
+import it.itsacademy.gestione_pagamento.entity.TipoPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface PagamentoRepository extends JpaRepository<Pagamento, UUID> {
 
-    List<Pagamento>findByIdOrdine(UUID idOrdine); //per legare pagamenti ad un ordine
+    List<Pagamento>findByIdOrdine(UUID idOrdine); //per legare pagamenti a un ordine
+
+    @Modifying //Attention, cette méthode ne va pas récupérer des données, elle va modifier la base de données (via un DELETE dans notre cas). Signification du @modifying ici.
+    @Transactional//car on doit le gérer dans une transcation
+    void deleteByStatoPagamento(TipoPagamento statoPagamento);
 
 }
