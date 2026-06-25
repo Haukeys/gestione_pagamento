b2f7ccbc69de54ece95ec98b36096165f16e24a5
Haukeys <leopazienza76@gmail.com>
2026-06-25 13:27:46 +0000

    FEAT: change naming and add new queue

--- src/main/java/it/itsacademy/gestione_pagamento/config/RabbitMQConfig.java
+++ src/main/java/it/itsacademy/gestione_pagamento/config/RabbitMQConfig.java
@@ -15,17 +15,40 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public Queue queue() {
-        return new Queue("exam_queue", true);
-    }
-
-    @Bean
     public TopicExchange exchange() {
-        return new TopicExchange("exam_exchange");
+        return new TopicExchange("orders.exchange");
+    }
+
+    // --- 1. CONFIGURATION FILE PAIEMENT ---
+    @Bean
+    public Queue paymentQueue() {
+        return new Queue("queue.pagamenti", true);
     }
 
     @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("exam_routingKey");
+    public Binding paymentBinding(Queue paymentQueue, TopicExchange exchange) {
+        return BindingBuilder.bind(paymentQueue).to(exchange).with("order.routing.payment");
+    }
+
+    // --- 2. CONFIGURATION FILE REÇU (JASPER) ---
+    @Bean
+    public Queue receiptQueue() {
+        return new Queue("queue.ricevute", true);
+    }
+
+    @Bean
+    public Binding bindingReceipt(Queue receiptQueue, TopicExchange exchange) {
+        return BindingBuilder.bind(receiptQueue).to(exchange).with("payment.routing.receipt");
+    }
+
+    // --- 3. CONFIGURATION FILE EMAIL ---
+    @Bean
+    public Queue emailQueue() {
+        return new Queue("queue.notifications", true);
+    }
+
+    @Bean
+    public Binding bindingEmail(Queue emailQueue, TopicExchange exchange) {
+        return BindingBuilder.bind(emailQueue).to(exchange).with("receipt.routing.email");
     }
 }
