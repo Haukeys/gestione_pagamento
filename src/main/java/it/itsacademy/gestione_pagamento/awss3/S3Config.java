b18edd61d00b337ec8291168ad9f375905db9976
Haukeys <leopazienza76@gmail.com>
2026-06-25 10:45:25 +0000

    FEAT: ADD NEW CONFIG FOR AWS S3

---
+++ src/main/java/it/itsacademy/gestione_pagamento/awss3/S3Config.java
@@ -0,0 +1,36 @@
+package it.itsacademy.gestione_pagamento.awss3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+
+import java.time.Duration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Value("${aws.s3.access-key}")
+    private String accessKey;
+
+    @Value("${aws.s3.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public S3AsyncClient s3AsyncClient() {
+        return S3AsyncClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                //Tempo di attesa massima per l'interazione con AWS
+                .overrideConfiguration(b -> b.apiCallTimeout(Duration.ofMinutes(2)))
+                .build();
+    }
+}
